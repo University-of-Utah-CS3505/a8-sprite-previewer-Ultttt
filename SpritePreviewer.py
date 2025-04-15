aa739ba1bd7ff45fdd6d63a81a75792ac9867338
Ultttt <135582372+Ultttt@users.noreply.github.com>
2025-04-15 15:38:35 -0600

    Initial commit

---
+++ SpritePreviewer.py
@@ -0,0 +1,120 @@
+import math
+
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+
+# This function loads a series of sprite images stored in a folder with a
+# consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
+def load_sprite(sprite_folder_name, number_of_frames):
+    frames = []
+    padding = math.ceil(math.log(number_of_frames - 1, 10))
+    for frame in range(number_of_frames):
+        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
+        frames.append(QPixmap(folder_and_file_name))
+
+    return frames
+
+class SpritePreview(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Sprite Preview")
+        # This loads the provided sprite and would need to be changed for your own.
+        self.num_frames = 20
+        self.frames = load_sprite('spriteImages',self.num_frames)
+
+        # Add any other instance variables needed to track information as the program
+        # runs here
+
+        self.timer = QTimer()
+        self.current_frame = 0
+        self.is_running = False
+
+        # Make the GUI in the setupUI method
+        self.setupUI()
+
+        self.slider.valueChanged.connect(self.update_fps_display)
+        self.start_button.clicked.connect(self.toggle_animation)
+        self.timer.timeout.connect(self.update_frame)
+
+    def update_fps_display(self):
+        fps = self.slider.value()
+        self.fps_value_label.setText(str(fps))
+
+    def toggle_animation(self):
+        if not self.is_running:
+            fps = self.slider.value()
+            delay = int(1000 / fps)
+            self.timer.start(delay)
+            self.start_button.setText("Stop")
+            self.is_running = True
+        else:
+            self.timer.stop()
+            self.start_button.setText("Start")
+            self.is_running = False
+
+    def update_frame(self):
+        self.current_frame = (self.current_frame + 1) % self.num_frames
+        self.image_label.setPixmap(self.frames[self.current_frame])
+
+
+    def setupUI(self):
+        # An application needs a central widget - often a QFrame
+        frame = QFrame()
+
+        # picture display
+        self.image_label = QLabel()
+        self.image_label.setPixmap(self.frames[0])
+
+        # FPS
+        self.fps_label = QLabel("Frames per second")
+        self.fps_value_label = QLabel("1")
+
+        # a slider
+        self.slider = QSlider(Qt.Orientation.Vertical)
+        self.slider.setRange(1, 20)
+        self.slider.setValue(1)
+
+        # a button
+        self.start_button = QPushButton('Start')
+
+        # Github Name
+        self.text = QLabel('Zhu&Dingyu')
+
+        # layout
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.text)
+        left_layout.addWidget(self.image_label)
+        left_layout.addWidget(self.fps_label)
+        left_layout.addWidget(self.start_button)
+
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.slider)
+        right_layout.addWidget(self.fps_value_label)
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(left_layout)
+        main_layout.addLayout(right_layout)
+
+        frame.setLayout(main_layout)
+        self.setCentralWidget(frame)
+
+
+
+    # You will need methods in the class to act as slots to connect to signals
+
+
+
+
+def main():
+    app = QApplication([])
+    # Create our custom application
+    window = SpritePreview()
+    # And show it
+    window.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
